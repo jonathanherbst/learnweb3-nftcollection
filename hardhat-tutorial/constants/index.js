a4be4102f2728781c205b9cee666500977f4197d
Jonathan Herbst <amd64d@gmail.com>
2022-07-04 16:20:52 -0400

    added api for opensea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x48F61b088f852081934295EC56A6c410080c36d3";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://learnweb3-nftcollection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
